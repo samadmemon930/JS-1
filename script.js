a226da0e170bbfb598340174a5865d88d3dd2a43
samadmemon930 <samadbusiness390@gmail.com>
2024-05-25 18:17:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// alert ("Hello")
+// alert ("I Am Samad")
+document.write("<h2>Today is our first class of JS<h2>")
+// String 
+// "String" , Text , sentence
+
+// number 
+//  1 2 3 0.3 -1 -2
+
+// boolean 
+// True , False
+
+// String
+var greeting ="Hello"
+alert (greeting);
+
+// Number
+//  var num1 = 5,
+// num2 = 100;
+// alert(num1+num2)
+
+// var num3 = 20;
+// num4 = 5;
+// alert(num3-num4)
+
+// var num5 = 8;
+// num6 = 7;
+// alert(num5*num6)
+
+// var num7 = 36;
+// num8 = 6;
+// document.write(num7/num8)
+
+
+ var num = 17;
+ var num1 = 7
+alert(2/ (num-num1) * 3)
+
+
+// illegal
+// var hello world = "hellow World"\
+
+var hello_world = "hello world"; //(underscore_case)//
+var helloWorld = "hello World";// (camelCase)//
+ 
+
+
+ 
+
+
+
+
+
